a15be78f7ba3e600823875aaa883a4f465c8e58e
Luke Tseng <lukegtseng@gmail.com>
2024-02-11 21:19:45 -0800

    Semaphore finished

---
+++ CS355/Semaphore/semaphore.cpp
@@ -0,0 +1,22 @@
+#include "semaphore.h"
+
+Semaphore::Semaphore(int count)
+{
+    this->count = count;
+}
+
+void Semaphore::wait()
+{
+    std::unique_lock<std::mutex> lock(m);
+    while (count == 0) {
+        cv.wait(lock);
+    }
+    count--;
+}
+
+void Semaphore::signal()
+{
+    std::unique_lock<std::mutex> lock(m);
+    count++;
+    cv.notify_one();
+}
